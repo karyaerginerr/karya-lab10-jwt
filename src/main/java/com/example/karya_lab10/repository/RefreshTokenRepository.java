60db0253272a087abfd53a2c67906092c773877b
karyaerginerr <karyaerginerr@gmail.com>
2026-01-27 03:20:20 +0100

    Add RefreshTokenRepository

---
+++ src/main/java/com/example/karya_lab10/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.karya_lab10.repository;
+
+import com.example.karya_lab10.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUserId(Long userId);
+}
